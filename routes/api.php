0c7d6e5a784617753470472dfef79a8f4b82f019
jefriwahyu <memberjefry1@gmail.com>
2026-07-30 09:03:55 +0700

    fix: routes/api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\OrderCOntroller;
+use App\Http\Controllers\Api\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
