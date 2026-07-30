f66af2f8e5be6efdf70f7c6910e2c0996400f4a6
jefriwahyu <memberjefry1@gmail.com>
2026-07-30 08:46:07 +0700

    fix: make api/index.php a self-contained Laravel front controller for Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,13 @@
-<?php 
-require __DIR__. '/../public/index.php';
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var Application $app */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
